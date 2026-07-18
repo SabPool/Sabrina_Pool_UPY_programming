04d9b01c3be6a513681eb8527de1ff06b39fdfb6
SabPool <2509273@upy.edu.mx>
2026-07-17 20:32:04 -0600

    Delete ejercicio1.py

--- ejercicio1.py
+++
@@ -1,7 +0,0 @@
-#aplicar año biciesto
-year = int(input("Enter a year: ")) #str
-
-if(year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
-    print(f"lap year {year}")
-else:
-    print("Nain")
