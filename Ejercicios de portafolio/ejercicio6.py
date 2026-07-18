15e06694053558ae5fbed423fbcd8f6263f12dd6
SabPool <2509273@upy.edu.mx>
2026-07-17 20:35:17 -0600

    ejercicios de 1 al 11

---
+++ Ejercicios de portafolio/ejercicio6.py
@@ -0,0 +1,14 @@
+#traffic light
+#red, yellow, green
+#red = stop, yellow = warning, green = go
+#any other color = invalid
+
+color= input("Ingresa un color: ").lower()
+if color == "red":
+    print("stop")
+elif color == "green"
+    print("go")
+elif color == "yellow":
+    print("warning")
+else:
+    print("Invalid")
